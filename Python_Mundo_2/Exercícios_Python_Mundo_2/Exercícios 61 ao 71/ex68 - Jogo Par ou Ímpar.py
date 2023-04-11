fa4c2b1c114d6f4a9889d663746c1f34e2fcf0c7
Lucas Henrique Costa de Oliveira <lucascosta13abc@gmail.com>
2023-04-10 21:31:06 -0300

    Exercício 68 Finalizado

---
+++ Python_Mundo_2/Exercícios_Python_Mundo_2/Exercícios 61 ao 71/ex68 - Jogo Par ou Ímpar.py
@@ -0,0 +1,31 @@
+# Crie um programa que jogue PAR OU ÍMPAR com o computador.
+# O jogo será interrompido quando o jogador PERDER, mostrando o total de vitórias
+# consecutivas que ele conquistou no final no jogo.
+
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(0, 11)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou Ímpar? [P/I] ')).upper()[0]
+    print(f'Você jogou {jogador} e o computador jogou {computador}. Total foi {total} ', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU ÍMPAR')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('VOCÊ VENCEU!')
+            v += 1
+        else:
+            print('VOCÊ PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('VOCÊ VENCEU!')
+            v += 1
+        else:
+            print('VOCÊ PERDEU!')
+            break
+    print('Vamos jogar novamente...')
+print(f'GAME OVER! Você venceu tantas {v} vezes.')
--- Python_Mundo_2/Exercícios_Python_Mundo_2/Exercícios 61 ao 71/ex68.py
+++
@@ -1,3 +0,0 @@
-# Crie um programa que jogue PAR OU ÍMPAR com o computador.
-# O jogo será interrompido quando o jogador PERDER, mostrando o total de vitórias
-# consecutivas que ele conquistou no final no jogo.
